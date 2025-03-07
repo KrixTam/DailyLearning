12b19f772607df8e0225a5d44b506c34c9a33b6a
Krix Tam <krix.tam@qq.com>
2025-03-07 16:45:00 +0800

    Ollma的学习方面增加两个样例： 函数调用的示例中，增加不进行转义的情况

--- 02_试验和实验/001_LLM/001_Ollama/001_Basic/example_03.py
+++ 02_试验和实验/001_LLM/001_Ollama/001_Basic/example_03.py
@@ -1,4 +1,4 @@
-# 函数调用的示例（进行转义的情况）
+# 函数调用的示例（不进行转义的情况）
 import ollama
 from utils import timer
 import requests
@@ -110,37 +110,13 @@ def demo():
                 'name': function_name,
                 'content': function_response
             })
-            example = '''
-            示例：
-                原数据：{'地址': '佛山市南海区', '时间': '2025-03-06', '天气': '阴', '实时温度': 10, '风向': '北风', '风力': '5-6级', '湿度': 77}
-                解读后：佛山市南海区今天的天气是阴天，当前温度10°C，吹北风，风力5-6级，湿度77%。
-            '''
-            query = f'''
-            {example}
-
-            请按照示例解读如下最新的天气情况，并反馈给用户：
-            {function_response}
-            '''
-            # print(query)
-            messages_02 = [
-                {
-                    'role': 'user',
-                    'content': query
-                }
-            ]
             res_02 = ollama.chat(
-                model='qwen2.5:7b',
-                # model='deepseek-r1:7b', # 没有必要用ds，用qw效率更高
-                messages=messages_02,
+                # model='qwen2.5:7b',
+                # model='mistral',
+                model='deepseek-r1:7b',
+                messages=messages_01,
             )
-            messages_02.append(res_02['message'])
-            messages_02.append(messages_01[0])
-            res_03 = ollama.chat(
-                model='qwen2.5:7b',
-                # model='deepseek-r1:7b',
-                messages=messages_02,
-            )
-            print(res_03['message']['content'])
+            print(res_02['message']['content'])
         else:
             print('Function', tool['function']['name'], 'not found')
 
