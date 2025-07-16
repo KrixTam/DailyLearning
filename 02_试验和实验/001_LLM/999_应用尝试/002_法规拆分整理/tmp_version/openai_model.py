73fa6039551e0cc7952251c5b3aa3bc84f85184a
Krix Tam <krix.tam@qq.com>
2025-07-17 06:48:13 +0800

    「法规拆分整理」试验，未实现TMP有效限流的版本归档

---
+++ 02_试验和实验/001_LLM/999_应用尝试/002_法规拆分整理/tmp_version/openai_model.py
@@ -0,0 +1,159 @@
+import os
+import asyncio
+import math
+from dataclasses import dataclass, field
+from typing import List, Dict, Optional
+import openai
+from openai import AsyncOpenAI, RateLimitError, APIConnectionError, APITimeoutError
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_exponential,
+    retry_if_exception_type,
+)
+from tokenizer import Tokenizer, Token
+from limitter import RPM, TPM
+from litellm import completion
+
+
+def get_top_response_tokens(response: openai.ChatCompletion) -> List[Token]:
+    token_logprobs = response.choices[0].logprobs.content
+    tokens = []
+    for token_prob in token_logprobs:
+        prob = math.exp(token_prob.logprob)
+        candidate_tokens = [
+            Token(t.token, math.exp(t.logprob))
+            for t in token_prob.top_logprobs
+        ]
+        token = Token(token_prob.token, prob, top_candidates=candidate_tokens)
+        tokens.append(token)
+    return tokens
+
+
+@dataclass
+class OpenAIModel:
+    model_name: str = "Qwen/Qwen3-8B"
+    api_key: str = None
+    base_url: str = "https://api.siliconflow.cn/v1"
+    # model_name: str = "qwen3:8b"
+    # api_key: str = "ollama"
+    # base_url: str = "http://localhost:11434/v1"
+
+    system_prompt: str = ""
+    json_mode: bool = False
+    seed: int = None
+    do_sample: bool = False
+    temperature: float = 0
+    max_tokens: int = 4096
+    repetition_penalty: float = 1.05
+    num_beams: int = 1
+    topk: int = 50
+    topp: float = 0.95
+
+    topk_per_token: int = 5  # number of topk tokens to generate for each token
+
+    token_usage: list = field(default_factory=list)
+    request_limit: bool = False
+    rpm: RPM = field(default_factory=lambda: RPM(rpm=1000))
+    tpm: TPM = field(default_factory=lambda: TPM(tpm=50000))
+
+    def __post_init__(self):
+        your_api_key = self.api_key
+        if your_api_key is None:
+            your_api_key = input("请输入你的API Key：")
+        self.client = AsyncOpenAI(api_key=your_api_key, base_url=self.base_url, timeout=180)
+        # os.environ["OPENAI_API_KEY"] = your_api_key
+        self.api_key = "API Key"
+
+    def _pre_generate(self, text: str, history: List[str]) -> Dict:
+        kwargs = {
+            "temperature": self.temperature,
+            "top_p": self.topp,
+            "max_tokens": self.max_tokens,
+        }
+        if self.seed:
+            kwargs["seed"] = self.seed
+        if self.json_mode:
+            kwargs["response_format"] = {"type": "json_object"}
+
+        messages = []
+        if self.system_prompt:
+            messages.append({"role": "system", "content": self.system_prompt})
+        messages.append({"role": "user", "content": text})
+
+        if history:
+            # assert len(history) % 2 == 0, "History should have even number of elements."
+            messages = history + messages
+
+        kwargs['messages'] = messages
+        return kwargs
+
+    @retry(
+        stop=stop_after_attempt(5),
+        wait=wait_exponential(multiplier=1, min=4, max=10),
+        retry=retry_if_exception_type((RateLimitError, APIConnectionError, APITimeoutError)),
+    )
+    async def generate_topk_per_token(self, text: str, history: Optional[List[str]] = None) -> List[Token]:
+        kwargs = self._pre_generate(text, history)
+        if self.topk_per_token > 0:
+            kwargs["logprobs"] = True
+            kwargs["top_logprobs"] = self.topk_per_token
+
+        # Limit max_tokens to 1 to avoid long completions
+        kwargs["max_tokens"] = 1
+
+        completion = await self.client.chat.completions.create(
+            model=self.model_name,
+            **kwargs
+        )
+
+        tokens = get_top_response_tokens(completion)
+
+        return tokens
+
+    @retry(
+        stop=stop_after_attempt(5),
+        wait=wait_exponential(multiplier=1, min=4, max=10),
+        retry=retry_if_exception_type((RateLimitError, APIConnectionError, APITimeoutError)),
+    )
+    async def generate(self, text: str, history: Optional[List[str]] = None, temperature: int = 0) -> str:
+        kwargs = self._pre_generate(text, history)
+        kwargs["temperature"] = temperature
+
+        prompt_tokens = 0
+        for message in kwargs['messages']:
+            prompt_tokens += len(Tokenizer().encode_string(message['content']))
+        estimated_tokens = prompt_tokens + kwargs['max_tokens']
+
+        if self.request_limit:
+            await self.rpm.wait(silent=True)
+            await self.tpm.wait(estimated_tokens, silent=True)
+
+        completion = await self.client.chat.completions.create(
+            model=self.model_name,
+            **kwargs
+        )
+        if hasattr(completion, "usage"):
+            self.token_usage.append({
+                "prompt_tokens": completion.usage.prompt_tokens,
+                "completion_tokens": completion.usage.completion_tokens,
+                "total_tokens": completion.usage.total_tokens,
+            })
+        return completion.choices[0].message.content
+
+
+async def main():
+    async def query(sem, llm, text):
+        async with sem:
+            completion = await llm.generate(text)
+            return completion
+    client = OpenAIModel()
+    semaphore = asyncio.Semaphore(3)
+    task = [query(semaphore, client, q) for q in ["推理模型会给市场带来哪些新的机会", "为什么天空是蓝色的？"]]
+    responses = await asyncio.gather(*task, return_exceptions=True)
+    for r in responses:
+        print(r)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
